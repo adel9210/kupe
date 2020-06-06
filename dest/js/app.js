f13534da7c22613a79bca7cad4f115507f9507b9
adel <adelabodoma@gmail.com>
2020-06-06 15:00:42 +0200

    product page

--- dest/js/app.js
+++ dest/js/app.js
@@ -1,4 +1,4 @@
-// toggle menu
+// Toggle menu in mobile view 
 Array.from(document.querySelectorAll('.toggle-side-menu'))
     .map(element => element.addEventListener('click', toggleSideMenu));
 
@@ -8,6 +8,39 @@ function toggleSideMenu() {
     document.querySelector('nav').classList.toggle('isMobile');
 }
 
+
+
+// Hide All Active When Click Outside 
+window.addEventListener('click', (event) => {
+    if (event.target.closest('.nav__link')) {
+        return ;
+    }
+    // Remove all active item 
+    const container = document.querySelectorAll('.nav__item--dropdown')
+    Array.from(container).map(ele => ele.classList.remove('u-d-block'));
+
+})
+
+// Toggle Nave item in Desktop View 
+Array.from(document.querySelectorAll('.nav__link')).map(li => {
+    li.addEventListener('click', toggleNavItem);
+});
+
+function toggleNavItem(e) {
+    // Remove all active item 
+    const container = document.querySelectorAll('.nav__item--dropdown')
+    Array.from(container).map(ele => ele.classList.remove('u-d-block'));
+
+
+    // Show Active Element
+    e.target.nextElementSibling.classList.toggle('u-d-block')
+
+    e.preventDefault()
+}
+
+
+
+// Hide Loader 
 window.addEventListener('load', () => {
     document.querySelector('.loader').classList.remove('show');
 })
